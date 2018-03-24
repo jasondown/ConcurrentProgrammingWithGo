1889bf67d86a3e95726283823659ddb7047a7767
Jason Down <jdown@igloosoftware.com>
2018-03-23 23:17:19 -0400

    Added 10ms sleep duration between hello and go message.

--- main.go
+++ main.go
@@ -6,15 +6,19 @@ import (
 
 func main() {
 
+	goDur, _ := time.ParseDuration("10ms")
+
 	go func() {
 		for i := 0; i < 100; i++ {
 			println("Hello")
+			time.Sleep(goDur)
 		}
 	}()
 
 	go func() {
 		for i := 0; i < 100; i++ {
 			println("Go")
+			time.Sleep(goDur)
 		}
 	}()
 
