b52d50167daa0c26aff5922184f757bea20e3af4
Jason Down <jdown@igloosoftware.com>
2018-03-29 00:07:53 -0400

    Added async logging via channel and go routine.

--- MutexLockWithGoRoutinesAndChannels/main.go
+++ MutexLockWithGoRoutinesAndChannels/main.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"runtime"
+	"time"
 )
 
 func main() {
 	runtime.GOMAXPROCS(4)
+
+	f, _ := os.Create("./log.txt")
+	f.Close()
+
+	logCh := make(chan string, 50)
+
+	go func() {
+		for {
+			msg, ok := <-logCh
+			if ok {
+				f, _ := os.OpenFile("./log.txt", os.O_APPEND, os.ModeAppend)
+				logTime := time.Now().Format(time.RFC3339)
+				f.WriteString(logTime + " - " + msg)
+				f.Close()
+			} else {
+				break
+			}
+		}
+	}()
 
 	mutex := make(chan bool, 1)
 
@@ -14,8 +35,10 @@ func main() {
 		for j := 1; j < 10; j++ {
 			mutex <- true
 			go func() {
-				fmt.Printf("%d + %d = %d\n", i, j, i+j)
-				<- mutex
+				msg := fmt.Sprintf("%d + %d = %d\n", i, j, i+j)
+				logCh <- msg
+				fmt.Print(msg)
+				<-mutex
 			}()
 		}
 	}
